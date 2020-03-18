266d523fd2c2af3c1d41468337a298c922f69864
Mike Miller <mmiller@levelsbeyond.com>
2020-03-18 00:11:00 -0600

    test in ff

--- assets/main.js
+++ assets/main.js
@@ -1,33 +1,36 @@
-$(function() {
+$(function () {
     // Set Zendesk SDK
     var client = ZAFClient.init();
+    
     // Garbage that will be tossed...Wait.
     function renderText(text) {
         var mainSectionEl = document.querySelector('section[data-main]');
         mainSectionEl.innerText = text;
     }
+    
     // Get current use...More or less used for testing...
     function getCurrentUser() {
-        return client.get('currentUser').then(function(data) {
+        return client.get('currentUser').then(function (data) {
             return data['currentUser'];
         });
     }
+    
     // Set AzureID in Zendesk
     // User still must submit the case - This *doesn't* auto save
     function setAzureID(ID) {
         client.set('ticket.customField:custom_field_360024830992', ID);
+        return ID;
     }
     
     // Let's get the current Azure ID Tied to this ticket
     function azureID() {
-        return client.get('ticket.customField:custom_field_360024830992').then(function(data) {
+        return client.get('ticket.customField:custom_field_360024830992').then(function (data) {
             var aid = data['ticket.customField:custom_field_360024830992'];
-            console.log('Azure ID is ' + aid );
+            console.log('Azure ID is ' + aid);
             if (aid !== null && aid !== '0' && aid !== '') {
                 var mainSectionEl = document.getElementById('CurrentLinkedInfo');
                 mainSectionEl.innerText = aid;
                 getTicket(aid);
-                azurePatch(aid);
                 var x = document.getElementById("Input");
                 if (x.style.display === "none") {
                     x.style.display = "block";
@@ -44,20 +47,21 @@ $(function() {
             }
         });
     }
+    
     // This gets the data from Azure
     // Need to work on getting more elements brought over. Only ticket type is done below for testing
     // I only get tickets under engineering..I could see this becoming an issue
-    function getTicket(ticket) {
-        var token ="TOKEN";
-        var org = "ORG";
-        var base64Pat = btoa(":"+token);
-        var request = new XMLHttpRequest;
-        console.log('Ticket data for ' + ticket);
-        request.open("GET","https://dev.azure.com/"+ org + "/engineering/_apis/wit/workitems?ids=" + ticket + "&api-version=2.0", true);
-        var b = "Basic " + base64Pat;
+    function getTicket(adticket) {
+        let token = "TOKEN";
+        let org = "ORG";
+        let base64Pat = btoa(":" + token);
+        let request = new XMLHttpRequest;
+        console.log('Ticket data for ' + adticket);
+        request.open("GET", "https://dev.azure.com/" + org + "/engineering/_apis/wit/workitems?ids=" + adticket + "&api-version=2.0", true);
+        let b = "Basic " + base64Pat;
         request.setRequestHeader("Authorization", b);
         
-        request.onload = function() {
+        request.onload = function () {
             var data = JSON.parse(JSON.stringify(this.response));
             var obj = JSON.parse(this.response);
             var mainSectionEl = document.getElementById('azureData-Type');
@@ -67,53 +71,65 @@ $(function() {
             var mainSectionEl2 = document.getElementById('azureData-Status');
             mainSectionEl2.innerText = JSON.stringify(obj.value[0].fields["System.State"]).replace(/['"]+/g, '');
             console.log(data);
-        }
+        };
         request.send();
     }
-
+    
     function azurePatch(aticket) {
-        return client.get('ticket.id').then(function(zid) {
+        console.log("Im azurePatch..");
+        return client.get('ticket.id').then(function (zid) {
+            console.log("Im azurePatch..I stop after this...");
             var zticket = zid['ticket.id'];
+            zticket = '"' + zticket + '"';
             console.log(zticket);
-            json = [
+            var json = [
                 {
                     "op": "add",
                     "path": "/fields/Custom.ZenDeskTicketNumber",
                     "value": zticket
                 }
-            ]
+            ];
+            json = JSON.stringify(json);
             // Put request
-            var token ="TOKEN";
-            var org = "ORG";
-            var base64Pat = btoa(":"+token);
-            fetch("https://dev.azure.com/"+ org + "/engineering/_apis/wit/workitems/" + aticket + "/bypassRules=true&api-version=5.1", {
-            method: 'PATCH',
-            body: JSON.stringify(json),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-                "Authorization": "Basic " + base64Pat
+            let token = "TOKEN";
+            let org = "ORG";
+            let base64Pat = btoa(":" + token);
+            let request = new XMLHttpRequest;
+            console.log('Ticket data for ' + aticket);
+            request.open("PUT", "https://dev.azure.com/" + org + "/_apis/wit/workitems/" + aticket + "/?bypassRules=true&api-version=5.1", true);
+            let b = "Basic " + base64Pat;
+            request.setRequestHeader("Authorization", b);
+            request.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+            request.onload = function () {
+                console.log(this.response);
             }
-        }).then(response => response.json()).then(json => console.log(json))
+            request.send(json);
+            
+        })
     }
-    )}    
+    
     //This does a thing right now. Still needs lots of love
     function init() {
-        getCurrentUser().then(function(currentUser) {
+        getCurrentUser().then(function (currentUser) {
             renderText('Hi ' + currentUser.name + ', trying to link to Azure? \n');
         });
         
-        var button = document.getElementById('submit')
-        button.addEventListener('click', function(){
-            var ticket = document.getElementById('Ticket').value;
-            setAzureID(ticket);
+        var button = document.getElementById('submit');
+        button.addEventListener('click', function () {
+            let ticket = document.getElementById('Ticket').value;
+            console.log("Running setAzureID");
+            azurePatch(setAzureID(ticket));
             window.location.reload(true);
         });
-        
         azureID();
+        
     }
+    
     // Size the widget
-    client.on('app.registered', function() {
-        client.invoke('resize', { width: '100%', height: '250px' });
+    client.on('app.registered', function () {
+        client.invoke('resize', {width: '100%', height: '250px'});
         init();
     });
-});
+}
+
+);
